22650bfedfb4e7e43299735e326dcb13709a08b6
andrehsym <kochetkovandre1993@gmail.com>
2022-01-15 22:20:41 +0300

    3 commit

---
+++ src/test/java/pro/sky/java/homeworks/course2/homework_11_re/Course2Homework11ReApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.homeworks.course2.homework_11_re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2Homework11ReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
